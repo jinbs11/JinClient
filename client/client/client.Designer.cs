1dd47e96d7979a23a2495b4983bb4dab868d36f5
Jino <jino.renman@gmail.com>
2025-04-28 22:31:27 +0300

    update

--- client/client/client.Designer.cs
+++ client/client/client.Designer.cs
@@ -26,14 +26,21 @@ namespace client
             buttonHolder = new Panel();
             settingsBTN = new Button();
             playBTN = new Button();
+            modMenuPanel = new Panel();
+            modPanel1 = new Panel();
+            modText1 = new Label();
+            modMenuCloseBTN = new Button();
             topPanel.SuspendLayout();
             buttonHolder.SuspendLayout();
+            modMenuPanel.SuspendLayout();
+            modPanel1.SuspendLayout();
             SuspendLayout();
             // 
             // clientName
             // 
             clientName.Anchor = AnchorStyles.None;
             clientName.AutoSize = true;
+            clientName.BackColor = Color.Transparent;
             clientName.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
             clientName.ForeColor = SystemColors.Control;
             clientName.Location = new Point(452, 49);
@@ -84,13 +91,58 @@ namespace client
             playBTN.UseVisualStyleBackColor = true;
             playBTN.Click += playBTN_Click;
             // 
+            // modMenuPanel
+            // 
+            modMenuPanel.BackColor = Color.Black;
+            modMenuPanel.Controls.Add(modPanel1);
+            modMenuPanel.Controls.Add(modMenuCloseBTN);
+            modMenuPanel.Dock = DockStyle.Bottom;
+            modMenuPanel.Location = new Point(0, 169);
+            modMenuPanel.Name = "modMenuPanel";
+            modMenuPanel.Size = new Size(1184, 442);
+            modMenuPanel.TabIndex = 3;
+            modMenuPanel.Visible = false;
+            // 
+            // modPanel1
+            // 
+            modPanel1.BackColor = Color.Gray;
+            modPanel1.Controls.Add(modText1);
+            modPanel1.Cursor = Cursors.Hand;
+            modPanel1.Location = new Point(12, 13);
+            modPanel1.Name = "modPanel1";
+            modPanel1.Size = new Size(200, 131);
+            modPanel1.TabIndex = 1;
+            // 
+            // modText1
+            // 
+            modText1.AutoSize = true;
+            modText1.BackColor = Color.Transparent;
+            modText1.Font = new Font("Segoe UI Semibold", 26.25F, FontStyle.Bold, GraphicsUnit.Point);
+            modText1.ForeColor = Color.Black;
+            modText1.Location = new Point(32, 35);
+            modText1.Name = "modText1";
+            modText1.Size = new Size(121, 47);
+            modText1.TabIndex = 0;
+            modText1.Text = "Skytils";
+            // 
+            // modMenuCloseBTN
+            // 
+            modMenuCloseBTN.Location = new Point(1097, 13);
+            modMenuCloseBTN.Name = "modMenuCloseBTN";
+            modMenuCloseBTN.Size = new Size(75, 23);
+            modMenuCloseBTN.TabIndex = 0;
+            modMenuCloseBTN.Text = "Close";
+            modMenuCloseBTN.UseVisualStyleBackColor = true;
+            modMenuCloseBTN.Click += modMenuCloseBTN_Click;
+            // 
             // client
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackgroundImage = Properties.Resources.clientBG;
+            BackColor = Color.FromArgb(64, 64, 64);
             BackgroundImageLayout = ImageLayout.Stretch;
             ClientSize = new Size(1184, 611);
+            Controls.Add(modMenuPanel);
             Controls.Add(buttonHolder);
             Controls.Add(topPanel);
             FormBorderStyle = FormBorderStyle.FixedDialog;
@@ -100,6 +152,9 @@ namespace client
             topPanel.ResumeLayout(false);
             topPanel.PerformLayout();
             buttonHolder.ResumeLayout(false);
+            modMenuPanel.ResumeLayout(false);
+            modPanel1.ResumeLayout(false);
+            modPanel1.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -110,5 +165,9 @@ namespace client
         private Panel buttonHolder;
         private Button settingsBTN;
         private Button playBTN;
+        private Panel modMenuPanel;
+        private Button modMenuCloseBTN;
+        private Panel modPanel1;
+        private Label modText1;
     }
 }
